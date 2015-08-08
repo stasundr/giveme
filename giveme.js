862a39ab865e3293f2c4146c4a3f850103ec54d4
Stanislav Dryomov <stasundr@air.local>
2015-08-08 00:03:29 -0400

    Blocks for Jackknife

--- giveme.js
+++ giveme.js
@@ -5,9 +5,7 @@ data.loadThreePopulationsGenotypes('LBK', 'MA1', 'Abkhasian', function(genotypes
 
     data.loadAllChromosomeRange(function(chromosomesRange) {
 
-        var noXYMT = function(genotype) {
-            return genotype.substr(0, chromosomesRange[22][1])
-        };
+        var noXYMT = function(genotype) { return genotype.substr(0, chromosomesRange[22][1]) };
         var frequency = function(a) {
             var f = [];
             var n = [];
@@ -37,29 +35,40 @@ data.loadThreePopulationsGenotypes('LBK', 'MA1', 'Abkhasian', function(genotypes
         var freq2 = frequency(genotypes2);
         var freq3 = frequency(genotypes3);
 
-        // f3 start
-        var t = 0;
-        var b = 0;
-        var s_c = 2 * genotypes3.length;
+        var f3 = function(start, end) {
+            var t = 0;
+            var b = 0;
+            var s_c = 2 * genotypes3.length;
 
-        for (var i = 0; i < freq3.length; i++) {
-            if ((freq1[i] > -1) && (freq2[i] > -1) && (freq3[i] > -1)) {
-                var n_c = 0;
-                var nC_c = 0;
+            for (var i = start; i < end; i++) {
+                if ((freq1[i] > -1) && (freq2[i] > -1) && (freq3[i] > -1)) {
+                    var n_c = 0;
+                    var nC_c = 0;
 
-                genotypes3.forEach(function(g) {
-                    if (g[i] == '0') n_c += 2;
-                    if (g[i] == '2') nC_c += 2;
-                    if (g[i] == '1') { n_c++; nC_c++ }
-                });
+                    genotypes3.forEach(function(g) {
+                        if (g[i] == '0') n_c += 2;
+                        if (g[i] == '2') nC_c += 2;
+                        if (g[i] == '1') { n_c++; nC_c++ }
+                    });
 
-                var ti = (freq3[i] - freq1[i]) * (freq3[i] - freq2[i]);
-                t += ti - n_c * nC_c / (s_c - 1) / s_c / s_c;
-                b += n_c * nC_c / (s_c - 1) / s_c;
+                    var ti = (freq3[i] - freq1[i]) * (freq3[i] - freq2[i]);
+                    t += ti - n_c * nC_c / (s_c - 1) / s_c / s_c;
+                    b += n_c * nC_c / (s_c - 1) / s_c;
+                }
             }
+
+            return t/b/2;
+        };
+        
+        var tmpf3 = [];
+        for (var i = 1; i <= 22; i++) {
+            tmpf3[i] = f3(chromosomesRange[i][0], chromosomesRange[i][1]);
+            console.log(i + ': ' + tmpf3[i]);
         }
-        console.log('f3: ' + t/b/2);
-        // f3 end
+
+        console.log(chromosomesRange);
+
+        console.log(f3(0, freq3.length));
 
         process.exit(0);
     });
